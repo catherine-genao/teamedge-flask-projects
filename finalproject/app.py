46aa48556f5d290d98283a8c0a4bb4101c1c4947
Catherine Genao <cgenao80@gmail.com>
2021-03-27 14:15:33 -0400

    finalproject  03/27/21

--- finalproject/app.py
+++ finalproject/app.py
@@ -1,31 +1,29 @@
 from flask import Flask, redirect,render_template, url_for, request
-from sense_emu import SenseHat   #import the SenseHat object from the sense_hat library
+from sense_hat import SenseHat   #import the SenseHat object from the sense_hat library
 from time import sleep
 sense = SenseHat()
 app = Flask(__name__)
 
 @app.route('/message')
 def index():
-    return render_template('index.html')
+    return render_template('message.html')
 
 @app.route('/success/<name>')
 def success(name):
    return 'welcome %s' % name
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/login',methods = ['POST' ])
 def login():
+   
    if request.method == 'POST':
-      user = request.form['nm']
+      user = request.form['name']
+      message = request.form['message']
+      sense.show_message(message)
       return redirect(url_for('success',name = user))
    else:
-      user = request.args.get('nm')
+      user = request.args.get('name')
       return redirect(url_for('success',name = user))
 
-@app.route('/success', methods=['GET','POST'])
-def success():
-   message = request.form['message']
-   sense.show_message(message)
-   return render_template('success.html', message = message)
 
 if __name__ == '__main__':
-      app.run(debug=True, host='127.0.0.4')
+      app.run(debug=True, host='127.0.0.1')
